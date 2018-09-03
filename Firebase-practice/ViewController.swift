d5c9d79df8a1a027b9a3e3b5b8ed0b97246d67b5
TaiHsin <peterlee0466@gmail.com>
2018-09-03 16:22:59 +0800

    Add and read data function

--- Firebase-practice/ViewController.swift
+++ Firebase-practice/ViewController.swift
@@ -8,19 +8,59 @@
 
 import UIKit
 import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
     
+    var ref: DatabaseReference!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        ref = Database.database().reference()
+        
+        setValue()
+//        readData()
     }
-
+    
+    func setValue() {
+        let uid = ref.child("users").childByAutoId().key
+        self.ref.child("users").child(uid).setValue(["email": "spock@gmail.com", "name": "Spock", "friends": ["Nia": true, "Crystal": true]])
+    }
+    
+    func readData() {
+        
+        ref.child("users").child("Nia").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            let userName = value?["name"] as? String
+            print(userName)
+            let userEmail = value?["email"] as? String
+            print(userEmail)
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
+    
+//    func updateData() {
+//        let key = ref.child("users").childByAutoId().key
+//        let post = ["uid": userID,
+//                    "author": username,
+//                    "title": title,
+//                    "body": body]
+//        let childUpdates = ["/posts/\(key)": post,
+//                            "/user-posts/\(userID)/\(key)/": post]
+//        ref.updateChildValues(childUpdates)
+//    }
+//
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
 }
 
