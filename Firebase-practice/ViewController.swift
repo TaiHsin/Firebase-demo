d4e0c2f2177ce514ea50f38320a4b539f03e9ea3
TaiHsin <peterlee0466@gmail.com>
2018-09-03 14:10:01 +0800

    Import Firebase SDK

---
+++ Firebase-practice/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Firebase-practice
+//
+//  Created by TaiHsinLee on 2018/9/3.
+//  Copyright © 2018年 TaiHsinLee. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
+
